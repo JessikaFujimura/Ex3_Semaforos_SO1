abe9ba96fb8e154c502cffb897395895fc644b02
Jéssika <jessikafujimura@gmail.com>
2020-09-18 20:41:13 -0300

    mostrando o tempo e o ranking de cada carro

---
+++ src/controller/ThreadCarro.java
@@ -0,0 +1,59 @@
+package controller;
+
+import java.util.Arrays;
+import java.util.concurrent.Semaphore;
+
+public class ThreadCarro extends Thread{
+	
+	private String idCarro;
+	private static int pos = 0;
+	private static int[] conjunto = new int[42];
+	private Semaphore semaforo;
+	
+	public ThreadCarro(String idCarro, Semaphore semaforo){
+		this.idCarro = idCarro;
+		this.semaforo = semaforo;
+	}
+	
+	@Override
+	public void run() {
+		try {
+			semaforo.acquire();
+			volta();
+			if(pos==42){
+				ranking();
+			}
+			
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		finally{
+			semaforo.release();
+		}
+
+	}
+
+	private void ranking() {
+		Arrays.sort(conjunto);
+		System.out.println("----------------RANKING----------------");
+		for(int t: conjunto){
+			System.out.println(t);
+		}
+	}
+
+	private void volta() {
+		for(int i = 1; i <= 3; i++){
+			int tempo = (int) (Math.random() *1001);
+			try {
+				sleep(tempo);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+			conjunto[pos] = tempo;
+			System.out.println(idCarro + " volta n° " + i + " tempo: " +  tempo + " ms");
+			pos++;
+		}
+		
+	}
+
+}
